d403e69659186a5a8acaed00cffc8f47f235d287
nertsal <nestralnertsal@gmail.com>
2024-02-29 11:27:17 +0300

    Setup geng

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,93 @@
+use geng::{prelude::*, Key, MouseButton};
+use geng_utils::{bounded::Bounded, conversions::*};
+
+pub type Color = Rgba<f32>;
+
+#[derive(clap::Parser)]
+struct Opts {
+    #[clap(flatten)]
+    geng: geng::CliArgs,
+}
+
+#[derive(geng::asset::Load)]
+pub struct Assets {
+    #[load(options(filter = "ugli::Filter::Nearest"))]
+    pub sword: Rc<ugli::Texture>,
+}
+
+struct State {
+    geng: Geng,
+    assets: Rc<Assets>,
+    framebuffer_size: vec2<usize>,
+    texture: ugli::Texture,
+    texture_target: Aabb2<f32>,
+}
+
+impl State {
+    pub fn new(geng: &Geng, assets: &Rc<Assets>) -> Self {
+        Self {
+            geng: geng.clone(),
+            assets: assets.clone(),
+            framebuffer_size: vec2(1, 1),
+            texture: {
+                let mut texture = geng_utils::texture::new_texture(geng.ugli(), vec2(640, 360));
+                texture.set_filter(ugli::Filter::Nearest);
+                texture
+            },
+            texture_target: Aabb2::ZERO,
+        }
+    }
+}
+
+impl geng::State for State {
+    fn update(&mut self, delta_time: f64) {
+        let delta_time = delta_time as f32;
+    }
+
+    fn handle_event(&mut self, event: geng::Event) {}
+
+    fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
+        self.framebuffer_size = framebuffer.size();
+        ugli::clear(framebuffer, Some(Color::BLACK), None, None);
+
+        let screen = Aabb2::ZERO.extend_positive(framebuffer.size().as_f32());
+        self.texture_target =
+            geng_utils::layout::fit_aabb(self.texture.size().as_f32(), screen, vec2(0.5, 0.5));
+
+        {
+            let framebuffer =
+                &mut geng_utils::texture::attach_texture(&mut self.texture, self.geng.ugli());
+            ugli::clear(framebuffer, Some(Color::BLACK), None, None);
+        }
+
+        let mut draw = geng_utils::texture::DrawTexture::new(&self.texture);
+        draw.target = self.texture_target;
+        draw.draw(&geng::PixelPerfectCamera, &self.geng, framebuffer);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    logger::init();
+    geng::setup_panic_handler();
+
+    let opts: Opts = clap::Parser::parse();
+
+    let mut options = geng::ContextOptions::default();
+    options.window.title = "Geng Game".to_string();
+    options.window.antialias = false;
+    // options.fixed_delta_time = 1.0 / FIXED_FPS;
+    options.with_cli(&opts.geng);
+
+    Geng::run_with(&options, move |geng| async move {
+        let manager = geng.asset_manager();
+        let assets_path = run_dir().join("assets");
+
+        let assets = <Assets as geng::asset::Load>::load(manager, &assets_path, &())
+            .await
+            .unwrap();
+        let assets = Rc::new(assets);
+
+        let state = State::new(&geng, &assets);
+        geng.run_state(state).await;
+    });
 }
