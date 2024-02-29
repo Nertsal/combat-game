391c720420a7f982433f76e7138192d095af5fb1
nertsal <nestralnertsal@gmail.com>
2024-02-29 11:45:25 +0300

    Cursor trail

--- src/game.rs
+++ src/game.rs
@@ -1,11 +1,30 @@
 use crate::prelude::*;
 
+use std::collections::VecDeque;
+
+const CURSOR_TRAIL_TIME: f32 = 0.4;
+const CURSOR_FADE_TIME: f32 = 0.2;
+
 pub struct State {
     pub geng: Geng,
     pub assets: Rc<Assets>,
     pub framebuffer_size: vec2<usize>,
     pub texture: ugli::Texture,
     pub texture_target: Aabb2<f32>,
+
+    pub camera: Camera2d,
+    /// World position of the cursor.
+    pub cursor_pos: vec2<Coord>,
+    pub cursor_history: VecDeque<CursorEntry>,
+    pub real_time: Time,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct CursorEntry {
+    /// World position of the cursor.
+    pub pos: vec2<Coord>,
+    /// Time at which the position was registered.
+    pub time: Time,
 }
 
 impl State {
@@ -20,16 +39,47 @@ impl State {
                 texture
             },
             texture_target: Aabb2::ZERO,
+
+            camera: Camera2d {
+                center: vec2::ZERO,
+                rotation: Angle::ZERO,
+                fov: 10.0,
+            },
+            cursor_pos: vec2::ZERO,
+            cursor_history: VecDeque::new(),
+            real_time: Time::ZERO,
         }
     }
 }
 
 impl geng::State for State {
     fn update(&mut self, delta_time: f64) {
-        let delta_time = delta_time as f32;
+        let delta_time = r32(delta_time as f32);
+        self.real_time += delta_time;
+
+        self.cursor_history
+            .retain(|entry| self.real_time - entry.time < r32(CURSOR_TRAIL_TIME));
     }
 
-    fn handle_event(&mut self, event: geng::Event) {}
+    fn handle_event(&mut self, event: geng::Event) {
+        match event {
+            geng::Event::CursorMove { position } => {
+                let position = position.as_f32();
+                let position = (position - self.texture_target.bottom_left())
+                    / self.framebuffer_size.as_f32()
+                    * self.texture.size().as_f32();
+                self.cursor_pos = self
+                    .camera
+                    .screen_to_world(self.texture.size().as_f32(), position)
+                    .as_r32();
+                self.cursor_history.push_back(CursorEntry {
+                    pos: self.cursor_pos,
+                    time: self.real_time,
+                });
+            }
+            _ => {}
+        }
+    }
 
     fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
         self.framebuffer_size = framebuffer.size();
@@ -43,6 +93,31 @@ impl geng::State for State {
             let framebuffer =
                 &mut geng_utils::texture::attach_texture(&mut self.texture, self.geng.ugli());
             ugli::clear(framebuffer, Some(Color::BLACK), None, None);
+
+            let camera = &self.camera;
+
+            let vertices = self
+                .cursor_history
+                .iter()
+                .map(|entry| {
+                    let t = (self.real_time - entry.time - r32(CURSOR_FADE_TIME)).max(Time::ZERO)
+                        / r32(CURSOR_TRAIL_TIME - CURSOR_FADE_TIME);
+                    let t = crate::util::smoothstep(t).as_f32();
+
+                    let color_a = Color::WHITE;
+                    let color_b = Color { a: 0.0, ..color_a };
+
+                    draw2d::ColoredVertex {
+                        a_pos: entry.pos.as_f32(),
+                        a_color: Color::lerp(color_a, color_b, t),
+                    }
+                })
+                .collect();
+            self.geng.draw2d().draw2d(
+                framebuffer,
+                camera,
+                &draw2d::Chain::new_gradient(vertices, 0.1, 0),
+            );
         }
 
         let mut draw = geng_utils::texture::DrawTexture::new(&self.texture);
--- src/prelude.rs
+++ src/prelude.rs
@@ -4,3 +4,5 @@ pub use geng_utils::{bounded::Bounded, conversions::*};
 pub use crate::assets::*;
 
 pub type Color = Rgba<f32>;
+pub type Time = R32;
+pub type Coord = R32;
